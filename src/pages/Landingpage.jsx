c167861be9d40c3598cb3d611d9c54dd3425f9ad
Sagar <sagar.kapoor@vyld.io>
2024-08-06 13:14:26 +0530

    done

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -22,7 +22,7 @@ const Landingpage = ({ skip, setskip }) => {
   };
   const triggerConfetti = () => {
     setShowConfetti(true);
-    setTimeout(() => setShowConfetti(false), 5000);
+    setTimeout(() => setShowConfetti(false), 2500);
   };
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -58,7 +58,7 @@ const Landingpage = ({ skip, setskip }) => {
   const [send, setsend] = useState(false);
 
   useEffect(() => {
-    if (right && send) {
+    if (send) {
       triggerConfetti();
     }
   }, [right, send]);
@@ -190,10 +190,10 @@ const Landingpage = ({ skip, setskip }) => {
           </svg>
         </div>
       }
-      {!send ? (
+  
         <div className="upper_buttons">
-          <button className="back_button">
-            <svg
+          <button className="back_button" onClick={()=>send && backward()}>
+            {!send?<svg
               width="24"
               height="24"
               viewBox="0 0 24 24"
@@ -206,47 +206,53 @@ const Landingpage = ({ skip, setskip }) => {
                 d="M13.165 11.9934L13.1634 11.6393C13.1513 10.2348 13.0666 8.98174 12.9206 8.18763C12.9206 8.17331 12.7613 7.38572 12.6599 7.12355C12.5006 6.74463 12.2126 6.42299 11.8515 6.2192C11.5624 6.0738 11.2592 6 10.9417 6C10.6922 6.01157 10.2806 6.13714 9.98692 6.24242L9.74283 6.33596C8.12612 6.97815 5.03561 9.07656 3.85199 10.3598L3.76473 10.4495L3.37527 10.8698C3.12982 11.1915 3 11.5847 3 12.0077C3 12.3866 3.11563 12.7656 3.3469 13.0718C3.41614 13.171 3.52766 13.2983 3.62693 13.4058L4.006 13.8026C5.31046 15.1243 8.13485 16.9782 9.59883 17.5924C9.59883 17.6057 10.5086 17.9857 10.9417 18H10.9995C11.6639 18 12.2846 17.6211 12.6021 17.0086C12.6888 16.8412 12.772 16.5132 12.8352 16.2252L12.949 15.6813C13.0788 14.8067 13.165 13.465 13.165 11.9934ZM19.4967 13.5183C20.3269 13.5183 21 12.8387 21 12.0004C21 11.1622 20.3269 10.4825 19.4967 10.4825L15.7975 10.8097C15.1463 10.8097 14.6183 11.3417 14.6183 12.0004C14.6183 12.6581 15.1463 13.1912 15.7975 13.1912L19.4967 13.5183Z"
                 fill="white"
               />
-            </svg>
+            </svg>:
+                <svg
+                width="26"
+                height="26"
+                viewBox="0 0 26 26"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="13"
+                  cy="13"
+                  r="12.25"
+                  stroke="white"
+                  stroke-width="1.5"
+                />
+                <path
+                  d="M9 9L17 17"
+                  stroke="white"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M17 9L9 17"
+                  stroke="white"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>}
           </button>
-          <button className="skip_button" onClick={() => setskip(true)}>
-            Skip
+          <button className="skip_button" onClick={() => !send?setskip(true):console.log("share")}>
+            {!send?'Skip':<svg width="38" height="38" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="16" cy="16" r="15.5" fill="#384353" stroke="#0E1928"/>
+<rect width="14" height="14" transform="translate(8 8)" fill="#384353"/>
+<path d="M19.7 13.4C21.1912 13.4 22.4 12.1912 22.4 10.7C22.4 9.20883 21.1912 8 19.7 8C18.2088 8 17 9.20883 17 10.7C17 10.8129 17.0069 10.9243 17.0204 11.0335L12.5743 13.2566C12.0887 12.7881 11.428 12.5 10.7 12.5C9.20883 12.5 8 13.7088 8 15.2C8 16.6912 9.20883 17.9 10.7 17.9C11.428 17.9 12.0888 17.6119 12.5744 17.1434L17.0204 19.3664C17.0069 19.4757 17 19.587 17 19.7C17 21.1912 18.2088 22.4 19.7 22.4C21.1912 22.4 22.4 21.1912 22.4 19.7C22.4 18.2088 21.1912 17 19.7 17C18.972 17 18.3113 17.2881 17.8257 17.7566L13.3796 15.5335C13.3931 15.4243 13.4 15.3129 13.4 15.2C13.4 15.087 13.3931 14.9757 13.3796 14.8664L17.8256 12.6434C18.3112 13.1119 18.972 13.4 19.7 13.4Z" fill="white"/>
+</svg>
+}
           </button>
         </div>
-      ) : (
+     
         <div className="upper_cross_button" onClick={() => backward()}>
           <button>
-            <svg
-              width="26"
-              height="26"
-              viewBox="0 0 26 26"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx="13"
-                cy="13"
-                r="12.25"
-                stroke="white"
-                stroke-width="1.5"
-              />
-              <path
-                d="M9 9L17 17"
-                stroke="white"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M17 9L9 17"
-                stroke="white"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+        
           </button>
         </div>
-      )}
+ 
       <div className="heading_above">
         <p className="you_got_a"><svg width="102" height="29" viewBox="0 0 102 29" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_1438_1502)">
@@ -310,7 +316,7 @@ const Landingpage = ({ skip, setskip }) => {
       )}
       {!send ? (
         <button
-          className={`${style?'SpotPage_Submit':'SpotPage_Submit2'}`}
+          className={`${style?'SpotPage_Submit button_setpos':'SpotPage_Submit2'}`}
           onClick={() => {
             forward();
           }}
@@ -318,10 +324,18 @@ const Landingpage = ({ skip, setskip }) => {
           <p>Send</p>
         </button>
       ) : (
-        <button className="SpotPage_Submit" onClick={() => backward()}>
-          <p>Close</p>
+        <div className="lower_bottom_buttons">
+          <button className="Create_Own">Create your own</button>
+        <button className="SpotPage_Submit" onClick={() => console.log("share")}>
+          <p>Challenge friends!</p>
         </button>
+        </div>
       )}
+      {<div className={`the_answer_reveal ${send?'Animation_div':'Animation_div2'}`}>
+        <p className="Your_guess">Your Guess</p>
+        <p className="Guess_is">{word}</p>
+        <button className="Guess_More_Button">Guess More &gt;</button>
+      </div>}
     </div>
   );
 };
